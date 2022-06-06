a3ef3cc1e5e6bbf8540a0bf2d5b41aa2be703eea
Joe Swantek <joseph.swantek@northwestern.edu>
2022-06-06 18:03:38 -0500

    SAGE-1354: simplified the github version extraction; remove `-f` option
    This was adopted from waggle_deb_builder to align all
    Waggle node Debian package builders to be the same.
    
    Reference:
    - https://github.com/waggle-sensor/waggle-deb-builder/blob/e8bcedbff563103899b666667c03d5d25457fff0/entrypoint.sh

--- build.sh
+++ build.sh
@@ -1,38 +1,26 @@
 #!/bin/bash -e
 
-print_help() {
-  echo """
-usage: build.sh [-f]
-Create the versioned Debian package.
- -f : force the build to proceed (debugging only) without checking for tagged commit
-"""
+get_version_short() {
+  git describe --tags --dirty 2>/dev/null || echo "v0.0.0"
 }
 
-FORCE=
-while getopts "f?" opt; do
-  case $opt in
-    f) # force build
-      echo "** Force build: ignore tag depth check **"
-      FORCE=1
-      ;;
-    ?|*)
-      print_help
-      exit 1
-      ;;
-  esac
-done
+get_version_long() {
+  sha=$(git rev-parse --short HEAD)
+  if [ -n "${sha}" ]; then
+    sha="-${sha}"
+  fi
+  git describe --tags --long --dirty 2>/dev/null || echo "v0.0.0${sha}"
+}
 
-# determine full version
-VERSION_SHORT=$(git describe --tags --dirty | cut -c2-)
-VERSION_LONG=$(git describe --tags --long --dirty | cut -c2-)
+trim_version_prefix() {
+  sed -e 's/^v//'
+}
 
-TAG_DEPTH=$(echo ${VERSION_LONG} | cut -d '-' -f 2)
-if [[ -z "${FORCE}" && "${TAG_DEPTH}_" != "0_" ]]; then
-  echo "Error:"
-  echo "  The current git commit has not been tagged. Please create a new tag first to ensure a proper unique version number."
-  echo "  Use -f to ignore error (for debugging only)."
-  exit 1
-fi
+# determine version
+VERSION_SHORT=$(get_version_short | trim_version_prefix)
+echo "VERSION_SHORT: ${VERSION_SHORT}"
+VERSION_LONG=$(get_version_long | trim_version_prefix)
+echo "VERSION_LONG: ${VERSION_LONG}"
 
 # get the list of all required debian packages to install in final image
 REQ_PACKAGES=$(sed -e '/^#/d' required_deb_packages.txt | tr '\n' ' ')
